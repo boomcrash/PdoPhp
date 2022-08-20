f565681e22bf033e7e2d10132256c526e50e9436
Pau <paulitayg@hotmail.com>
2022-08-20 15:00:06 -0500

    Consultar

--- envioInternacional/consultar.php
+++ envioInternacional/consultar.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Yánez Guillén Paula Adriana">
-        <title>CONSULTAR - APELLIDO</title>
+        <title>CONSULTAR - YÁNEZ</title>
     </head>
     <body>
 
@@ -23,10 +23,59 @@
 
         <!-- TU CODIGO EMPIEZA AQUI -->
               
+        require_once '../conexion.php';
+
+$sql = "select * from envio_internacional";
+$stmt = $pdo->prepare($sql);// preparar la sentencia
+$stmt->execute(); // ejecutar la sentencia
+?>
+
+<div>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>Telefonos</th>
+                <th>EMAIL</th>
+                <th>Direccion</th>
+                <th>Recibir vía</th>
+                <th>Pais</th>
+                <th>Información</th>
+                <th>Especificaciones</th>
+
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $filas = $stmt->fetchAll(PDO::FETCH_ASSOC); // recuperar resultados
+            foreach ($filas as $fila) {
+                ?>
+                <tr>
+                    <td><?php echo $fila['internacional_id'] ?></td>
+                    <td><?php echo $fila['nombres'] ?></td>
+                    <td><?php echo $fila['apellidos'] ?></td>
+                    <td><?php echo $fila['telefono'] ?></td>
+                    <td><?php echo $fila['email'] ?></td>
+                    <td><?php echo $fila['direccion'] ?></td>
+                    <td><?php echo $fila['recibir_via'] ?></td>
+                    <td><?php echo $fila['pais'] ?></td>
+                    <td><?php echo $fila['recibir_info'] ?></td>
+                    <td><?php echo $fila['especificaciones'] ?></td>
 
 
 
-
+                    <td>
+                        <a href="eliminar.php?id=<?php echo $fila['id'] ?>">Eliminar</a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    <a href="agregar.php">Agregar</a>
+</div>
 
 
         <!-- TU CODIGO TERMINA AQUI -->
