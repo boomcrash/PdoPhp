597c8dbcf97454185782a940fb9533618fa8cd7c
EMA <emelyapraez25@gmail.com>
2022-08-19 21:54:34 -0500

    Meta author

--- resenias/eliminar.php
+++ resenias/eliminar.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="Emely Mishell Apráez González">
+        <meta name="author" content="Apráez González Emely Mishell">
         <title>ELIMINAR - APRAEZ</title>
     </head>
     <body>
