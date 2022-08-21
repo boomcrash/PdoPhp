d135072079f20a18f15314a92c650d24922497b8
Pau <paulitayg@hotmail.com>
2022-08-21 13:13:35 -0500

    EDICION

--- envioInternacional/insertar.php
+++ envioInternacional/insertar.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="author" content="Yánez Guillén Paula Adriana">
-		<title>INSERTAR - APELLIDO</title>
+		<title>INSERTAR - YÁNEZ</title>
 		<style>
 
 .formularios div {
